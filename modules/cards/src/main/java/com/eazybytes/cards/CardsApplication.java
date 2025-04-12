68290cecc097db396ba6cfbb0b86447db2d9559e
enzoboto1 <enzoboto1.0@gmail.com>
2025-04-11 23:22:36 -0300

    ajustando server port de cards

--- modules/cards/src/main/java/com/eazybytes/cards/CardsApplication.java
+++ modules/cards/src/main/java/com/eazybytes/cards/CardsApplication.java
@@ -9,34 +9,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 /*@ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
 @EnableJpaRepositories("com.eazybytes.cards.repository")
 @EntityScan("com.eazybytes.cards.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
-		info = @Info(
-				title = "Cards microservice REST API Documentation",
-				description = "EazyBank Cards microservice REST API Documentation",
-				version = "v1",
-				contact = @Contact(
-						name = "Madan Reddy",
-						email = "tutor@eazybytes.com",
-						url = "https://www.eazybytes.com"
-				),
-				license = @License(
-						name = "Apache 2.0",
-						url = "https://www.eazybytes.com"
-				)
-		),
-		externalDocs = @ExternalDocumentation(
-				description = "EazyBank Cards microservice REST API Documentation",
-				url = "https://www.eazybytes.com/swagger-ui.html"
-		)
+        info = @Info(
+                title = "Cards microservice REST API Documentation",
+                description = "EazyBank Cards microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Madan Reddy",
+                        email = "tutor@eazybytes.com",
+                        url = "https://www.eazybytes.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.eazybytes.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Cards microservice REST API Documentation",
+                url = "https://www.eazybytes.com/swagger-ui.html"
+        )
 )
 public class CardsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(CardsApplication.class);
+        Map<String, Object> props = new HashMap<>();
+        props.put("server.port", 9000);
+        app.setDefaultProperties(props);
+        app.run(args);
+    }
 }
